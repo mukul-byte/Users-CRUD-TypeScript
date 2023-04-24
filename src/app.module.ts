83727e34857520964dabde21356e3321b050ba62
Mukul Goyal <mukulgoyal@Mukuls-MacBook-Air.local>
2023-04-24 15:43:51 +0530

    add user resource & DB connection

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: '',
+      database: 'users',
+      entities: [User],
+      synchronize: true,
+    }), 
+    UserModule,
+  ],
+
+}) 
+export class AppModule {}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    age: number;
+}

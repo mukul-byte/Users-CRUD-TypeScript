010b091b2d62ed24f09bc85c6863c6b81635f32f
Mukul Goyal <mukulgoyal@Mukuls-MacBook-Air.local>
2023-04-25 15:20:41 +0530

    add custom provider and export it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
@@ -20,17 +20,22 @@ import { PersonProvider } from './di/providers/person.provider';
       synchronize: true,
     }),
     UserModule,
-    DependencyModule
+    DependencyModule,
+    
   ],
 
 }) 
 export class AppModule {
   constructor(private readonly log: LoggingProvider,
-    private readonly person: PersonProvider){
+    private readonly person: PersonProvider,
+    @Inject("APP_CONSTANTS") private readonly constants: any
+    ){
     log.loggError("this is an error msg");
     log.loggInfo("this is an error msg");
     log.loggSuccess("this is an error msg");
 
     console.log(person.getPerson());
+
+    console.log("In AppModule" + constants.DB_NAME + " " + constants.DB_PASSWORD);
   }
 }
--- src/di/dependency.module.ts
+++ src/di/dependency.module.ts
@@ -1,20 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { LoggingProvider } from './providers/logging.provider';
 import { PersonProvider } from './providers/person.provider';
 
+const CONSTANTS = {
+  DB_NAME: "Users",
+  DB_PASSWORD: "admin@123"
+}
+
 @Module({
   controllers: [],
-  providers: [LoggingProvider, PersonProvider],
+  providers: [LoggingProvider, PersonProvider,
+    {useValue:CONSTANTS, provide: "APP_CONSTANTS"}
+  ],
   imports: [],
-  exports: [LoggingProvider, PersonProvider]
+  exports: [LoggingProvider, PersonProvider, "APP_CONSTANTS"]
 })
-export class DependencyModule { 
+export class DependencyModule {
   constructor(private readonly log: LoggingProvider,
-              private readonly person: PersonProvider){
+    private readonly person: PersonProvider,
+    @Inject("APP_CONSTANTS") private readonly constants: any
+    ) {
     log.loggError("this is an error msg");
     log.loggInfo("this is an error msg");
     log.loggSuccess("this is an error msg");
 
     console.log(person.getPerson());
+    console.log(constants.DB_NAME + " " + constants.DB_PASSWORD);
   }
 }
